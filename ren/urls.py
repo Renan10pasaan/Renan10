64c2904ac14fec7e189a61f88828dc0c23fd59db
KIER <tomino.k.bsinfotech@gmail.com>
2024-04-11 12:15:26 +0800

    Second Commit

---
+++ ren/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    #path('login/', views.login_user, name='login'),
+
+]
+   
